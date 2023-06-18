dbb5685181afbc08c4ceb74f6e396340164ff331
maruchekas <salken84@gmail.com>
2023-06-18 13:25:57 +0300

    updated config, added lombok config

--- src/main/java/ru/maruchek/kafkademo/servise/MessageConsumer.java
+++ src/main/java/ru/maruchek/kafkademo/servise/MessageConsumer.java
@@ -7,13 +7,11 @@ import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
-import static ru.maruchek.kafkademo.config.KafkaConsumerConfig.TOPIC_NAME;
-
 @Slf4j
 @Service
 public class MessageConsumer {
 
-    @KafkaListener(topics = TOPIC_NAME)
+    @KafkaListener(topics = "${kafka.topic.name}")
     public void listen(@Payload String message,
                        @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition) {
         log.info("Receive message: {} in partition {}", message, partition);
